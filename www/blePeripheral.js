ebec9c5991edcfd7c964fed8a0b539c809a9dd09
Batraz Dz <batik611@ya.ru>
2024-10-18 07:38:23 +0300

    feat: add some methods

--- www/blePeripheral.js
+++ www/blePeripheral.js
@@ -17,7 +17,7 @@
 
 // Util functions for translating nested array buffers going across the Cordova bridge
 
-var stringToArrayBuffer = function(str) {
+var stringToArrayBuffer = function (str) {
     var ret = new Uint8Array(str.length);
     for (var i = 0; i < str.length; i++) {
         ret[i] = str.charCodeAt(i);
@@ -26,7 +26,7 @@ var stringToArrayBuffer = function(str) {
     return ret.buffer;
 };
 
-var base64ToArrayBuffer = function(b64) {
+var base64ToArrayBuffer = function (b64) {
     return stringToArrayBuffer(atob(b64));
 };
 
@@ -43,7 +43,7 @@ function convertToNativeJS(object) {
     Object.keys(object).forEach(function (key) {
         var value = object[key];
         object[key] = massageMessageNativeToJs(value);
-        if (typeof(value) === 'object') {
+        if (typeof (value) === 'object') {
             convertToNativeJS(value);
         }
     });
@@ -52,21 +52,22 @@ function convertToNativeJS(object) {
 // end Util functions
 
 var onWriteRequestCallback;
+var onReadRequestCallback;
 var onBluetoothStateChangeCallback;
 
 function registerWriteRequestCallback() {
 
-    var didReceiveWriteRequest = function(json) {
-      console.log('didReceiveWriteRequest');
-      console.log(json);
-      convertToNativeJS(json);
+    var didReceiveWriteRequest = function (json) {
+        console.log('didReceiveWriteRequest');
+        console.log(json);
+        convertToNativeJS(json);
 
-      if (onWriteRequestCallback && typeof onWriteRequestCallback === 'function') {
-        onWriteRequestCallback(json);
-      }
+        if (onWriteRequestCallback && typeof onWriteRequestCallback === 'function') {
+            onWriteRequestCallback(json);
+        }
     };
 
-    var failure = function() {
+    var failure = function () {
         // this should never happen
         console.log("Failed to add setCharacteristicValueChangedListener");
     };
@@ -75,17 +76,56 @@ function registerWriteRequestCallback() {
 }
 registerWriteRequestCallback();
 
+function registerReadRequestCallback() {
+
+    var didReceiveReadRequest = function(dictionaryObject) {
+        console.log('didReceiveReadRequest', dictionaryObject);
+        
+        const contextID = dictionaryObject.contextID;
+        const characteristic = dictionaryObject.characteristic;
+        const service = dictionaryObject.service;
+        
+        if (!(contextID && characteristic && service)) {
+            console.log('didReceiveReadRequest', 'malformed reqeust');
+            return
+        }
+        
+        if (onReadRequestCallback && typeof onReadRequestCallback === 'function') {
+            let result = onReadRequestCallback(service, characteristic);
+            
+            if(result) {
+                if (result instanceof ArrayBuffer) {
+                    var base64String = btoa([].reduce.call(new Uint8Array(result),function(p,c){return p+String.fromCharCode(c)},''));
+                    cordova.exec(() => { }, () => { }, 'BLEPeripheral', 'receiveRequestedCharacteristicValue', [contextID, base64String]);
+                    
+                } else if (typeof result === 'string') {
+                    var base64String = btoa(result);
+                    cordova.exec(() => { }, () => { }, 'BLEPeripheral', 'receiveRequestedCharacteristicValue', [contextID, base64String]);
+                }
+
+            }
+        }
+    };
+    
+    var failure = function () {
+        // this should never happen
+        console.log("Failed to add setCharacteristicValueRequestedListener");
+    };
+    
+    cordova.exec(didReceiveReadRequest, failure, 'BLEPeripheral', 'setCharacteristicValueRequestedListener');
+}
+
 function registerBluetoothStateChangeCallback() {
 
-    var bluetoothStateChanged = function(state) {
-      console.log('bluetoothStateChanged', state);
+    var bluetoothStateChanged = function (state) {
+        console.log('bluetoothStateChanged', state);
 
-      if (onBluetoothStateChangeCallback && typeof onBluetoothStateChangeCallback === 'function') {
-        onBluetoothStateChangeCallback(state);
-      }
+        if (onBluetoothStateChangeCallback && typeof onBluetoothStateChangeCallback === 'function') {
+            onBluetoothStateChangeCallback(state);
+        }
     };
 
-    var failure = function() {
+    var failure = function () {
         // this should never happen
         console.log("Failed to add bluetoothStateChangedListener");
     };
@@ -94,15 +134,15 @@ function registerBluetoothStateChangeCallback() {
 }
 registerBluetoothStateChangeCallback();
 
-/* 
+/*
 Characteristic premissions are not consistent across platforms. This will need to be reconciled.
 Maybe permissions should be optional and default to read/write based on the properties.
 
 // iOS permissions CBCharacteristic.h
-    CBAttributePermissionsReadable					= 0x01,
-    CBAttributePermissionsWriteable					= 0x02,
-    CBAttributePermissionsReadEncryptionRequired	= 0x04,
-    CBAttributePermissionsWriteEncryptionRequired	= 0x08
+    CBAttributePermissionsReadable                    = 0x01,
+    CBAttributePermissionsWriteable                    = 0x02,
+    CBAttributePermissionsReadEncryptionRequired    = 0x04,
+    CBAttributePermissionsWriteEncryptionRequired    = 0x08
 
 // Android permissions BluetoothGattCharacteristic.java
 
@@ -118,7 +158,7 @@ Maybe permissions should be optional and default to read/write based on the prop
 
 module.exports = {
 
-    properties : {
+    properties: {
         READ: 0x02,
         WRITE: 0x08,
         WRITE_NO_RESPONSE: 0x04,
@@ -130,79 +170,101 @@ module.exports = {
         READABLE: 0x01,
         WRITEABLE: cordova.platformId === 'ios' ? 0x02 : 0x10,
         READ_ENCRYPTION_REQUIRED: cordova.platformId === 'ios' ? 0x04 : 0x02,
-        WRITE_ENCRYPTION_REQUIRED: cordova.platformId === 'ios' ? 0x08: 0x20
+        WRITE_ENCRYPTION_REQUIRED: cordova.platformId === 'ios' ? 0x08 : 0x20
     },
 
-    createService: function(uuid) {
+    createService: function (uuid) {
 
-        return new Promise(function(resolve, reject) {
-             cordova.exec(resolve, reject, 'BLEPeripheral', 'createService', [uuid]);
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'createService', [uuid]);
         });
     },
 
-    createServiceFromJSON: function(json) {
+    createServiceFromJSON: function (json) {
 
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             cordova.exec(resolve, reject, 'BLEPeripheral', 'createServiceFromJSON', [json]);
         });
 
     },
 
-    addCharacteristic: function(service, characteristic, properties, permissions) {
+    addCharacteristic: function (service, characteristic, properties, permissions) {
 
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             cordova.exec(resolve, reject, 'BLEPeripheral', 'addCharacteristic', [service, characteristic, properties, permissions]);
         });
 
     },
 
-    addService: function(service) {
+    publishService: function (uuid) {
 
-        return new Promise(function(resolve, reject) {
-            cordova.exec(resolve, reject, 'BLEPeripheral', 'addService', [service]);
-        });
-
-    },
-
-    publishService: function(uuid) {
-
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             cordova.exec(resolve, reject, 'BLEPeripheral', 'publishService', [uuid]);
         });
 
     },
 
-    // Future versions should should allow one or multiple services, name should be optional
-    startAdvertising: function(service, localName) {
+    removeService: function (service) {
 
-        return new Promise(function(resolve, reject) {
-            cordova.exec(resolve, reject, 'BLEPeripheral', 'startAdvertising', [service, localName]);
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'removeService', [service]);
+        });
+
+    },
+
+    removeAllServices: function () {
+
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'removeAllServices', []);
+        });
+
+    },
+
+    // Future versions should should allow one or multiple services, name should be optional
+    startAdvertising: function (localName, service) {
+
+        return new Promise(function (resolve, reject) {
+            // var param = []
+            // param.push(localName)
+            // for (i=0; i<services.length; i++) {
+            //     param.push(services[i])
+            // }
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'startAdvertising', [localName, service]);
+        });
+
+    },
+
+    stopAdvertising: function () {
+
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'stopAdvertising', []);
         });
 
     },
 
     // setting the value automatically notifies subscribers
-    setCharacteristicValue: function(service, characteristic, value) {
+    setCharacteristicValue: function (service, characteristic, value) {
 
-        return new Promise(function(resolve, reject) {
-            if (value.constructor !== ArrayBuffer) {
-                // TODO try calling value.buffer before rejecting
+        return new Promise(function (resolve, reject) {
+            var buffer = ArrayBuffer.isView(value) ? value.buffer : value;
+
+            if (buffer.constructor.name !== 'ArrayBuffer') {
                 reject('value must be an ArrayBuffer');
             }
-            cordova.exec(resolve, reject, 'BLEPeripheral', 'setCharacteristicValue', [service, characteristic, value]);
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'setCharacteristicValue', [service, characteristic, buffer]);
         });
 
     },
 
     // setDescriptorValue: function(service, characteristic, descriptor, value) {
-    // 
+    //
     //     return new Promise(function(resolve, reject) {
     //         if (value.constructor !== ArrayBuffer) {
     //             reject('value must be an ArrayBuffer');
     //         }
     //         cordova.exec(resolve, reject, 'BLEPeripheral', 'setDescriptorValue', [service, characteristic, descriptor, value]);
     //     });
-    // 
+    //
     // },
 
     // setDescriptorValueChangedListener: function(success) {
@@ -222,12 +284,34 @@ module.exports = {
     //   characteristic: '5678',
     //   value: someArrayBuffer
     // }
-    onWriteRequest: function(callback) {
+    onWriteRequest: function (callback) {
         onWriteRequestCallback = callback;
     },
+    
+    onReadRequest: function (callback) {
+        onReadRequestCallback = callback;
+        registerReadRequestCallback();
+    },
 
-    onBluetoothStateChange: function(callback) {
+    onBluetoothStateChange: function (callback) {
         onBluetoothStateChangeCallback = callback;
-    }
+    },
 
+    //If miniapp uses background features, services and advertising is performed even when miniapp is closed, or even applicaiton is closed.
+    //At some point (on read or write request from some bluetooth central) OS launches app, then app launches miniapp
+    //You should call this function when your miniapp is ready to accept such (read & write) events.
+    //You should call this function asap, because such bluetooth requests have 3 seconds timeout (OS restrictions),
+    //and some of this time is usually already consumed by starting the miniapp.
+    startSendingStashedReadWriteEvents: function() {
+        cordova.exec(() => {}, () => {}, 'BLEPeripheral', 'startSendingStashedReadWriteEvents', []);
+    },
+
+    //If miniapp uses background features, services and advertising is performed even when miniapp is closed, or even applicaiton is closed.
+    //So, in context of miniapp it may be dificult to understand, in what state is adverting and services are at the moment (at least aon the start of the miniapp)
+    //For that we made this fucntion, which exports data in json
+    getBluetoothSystemState: function() {
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'BLEPeripheral', 'getBluetoothSystemState', []);
+        });
+    }
 };
